6fd12c0250dd240bd1c7ede9a00a3957c545ccd4
Benjamin Sweeney <bends11@vt.edu>
2016-04-12 11:39:58 -0400

    Add implemented ApiManager

--- src/java/com/mycompany/virtualtickets/ApiManager.java
+++ src/java/com/mycompany/virtualtickets/ApiManager.java
@@ -4,10 +4,66 @@
  */
 package com.mycompany.virtualtickets;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  *
  * @author painter
  */
 public class ApiManager {
-    
+    /**
+     *
+     * @param title
+     * @return
+     */
+    public Movie searchOmdb(String title) {
+        Movie mov = null;
+
+        String url = "http://www.omdbapi.com/?t="
+                + title + "&y=&plot=full&r=json";
+
+        try {
+            JSONObject json = readJsonFromUrl(url);
+            mov = new Movie (
+            json.getString("title"),
+            json.getInt("year"), 
+            json.getString("rated"),
+            json.getString("released"),
+            json.getString("runtime"),
+            json.getString("metascore"),
+            json.getString("imbdscore"));
+        } catch (Exception e) {
+            return mov;
+        }
+        return mov;
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
 }
---
+++ src/java/com/mycompany/virtualtickets/Movie.java
@@ -0,0 +1,92 @@
+/*
+ * Created by Nicholas Phillpott on 2016.04.12  * 
+ * Copyright © 2016 Nicholas Phillpott. All rights reserved. * 
+ */
+package com.mycompany.virtualtickets;
+
+/**
+ *
+ * @author painter
+ */
+public class Movie {
+    private String title; 
+    private int year; 
+    private String rated; 
+    private String released; 
+    private String runtime; 
+    private String metascore; 
+    private String imdbRating; 
+    
+    public Movie() {
+        
+    }
+
+    public Movie(String title, int year, String rated, String released, String runtime, String metascore, String imdbRating) {
+        this.title = title;
+        this.year = year;
+        this.rated = rated;
+        this.released = released;
+        this.runtime = runtime;
+        this.metascore = metascore;
+        this.imdbRating = imdbRating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getRated() {
+        return rated;
+    }
+
+    public void setRated(String rated) {
+        this.rated = rated;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getMetascore() {
+        return metascore;
+    }
+
+    public void setMetascore(String metascore) {
+        this.metascore = metascore;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(String imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+    
+    
+    
+}
