70531e3e1e9f30241b45882f4670adcbdc6bf3e6
Imploding40 <Imploding40@PillarOfAutumn>
2016-04-30 14:56:31 -0400

    Trying to revert

--- src/java/com/mycompany/managers/LoginManager.java
+++ src/java/com/mycompany/managers/LoginManager.java
@@ -101,7 +101,7 @@ public class LoginManager implements Serializable {
     User user = userFacade.findByUsername(getUsername());
     if (user == null) {
       errorMessage = "Invalid username or password!";
-      return null;
+      return "";
     } else {
       if (user.getUsername().equals(getUsername()) && user.getPassword().equals(getPassword())) {
         errorMessage = "";
